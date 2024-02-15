5244ca34c81ee09a31664ff3e5769ce803b03730
Sergio <git@sergio.rebelo.com>
2024-02-15 17:17:37 +0000

    update on the migration scripts to create user profiles on users created in previous versions

---
+++ usermanagement/src/main/resources/db/migration/V2__data_migration.sql
@@ -0,0 +1,6 @@
+-- Create default profiles for users without profiles
+INSERT INTO user_profiles (user_id)
+SELECT u.id
+FROM users u
+LEFT JOIN user_profiles up ON u.id = up.user_id
+WHERE up.user_id IS NULL;
